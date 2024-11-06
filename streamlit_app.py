6a48b34eda03d789183b0e7ba5045b5323ce33e2
AkGangal22 <83156908+AkGangal22@users.noreply.github.com>
2024-11-06 17:49:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your smoothie! :cup_with_straw:")
@@ -15,8 +16,13 @@ st.write('The name on your Smoothie will be: ',name_on_the_order)
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON')
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
+                                                                      
 
 ingredients_list = st.multiselect('Choose upto 5 Ingredients',my_dataframe,max_selections=5)
 if ingredients_list:
@@ -26,7 +32,12 @@ if ingredients_list:
     ingredients_string=''
     for fruit_choosen in ingredients_list:
         ingredients_string+=fruit_choosen+' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
+        st.subheader(fruit_choosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choosen)
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
